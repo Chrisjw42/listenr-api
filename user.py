7e8bd76c7eeb6f27954e3a7f680e5ff751921e62
Chrisjw42 <chrisjw42@gmail.com>
2019-01-06 21:16:03 +1100

    adding user and rq

---
+++ user.py
@@ -0,0 +1,28 @@
+import base64
+from datetime import datetime, timedelta
+import os
+
+class User():
+    # ...
+    token = ""
+    token_expiration = datetime.now() - timedelta(seconds=1)
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        
+        # Generate a new token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+#     @staticmethod
+    def check_token(self, token):
+#         user = User.query.filter_by(token=token).first()
+        if token != self.token or self.token_expiration < datetime.utcnow():
+            return None
+        return self
